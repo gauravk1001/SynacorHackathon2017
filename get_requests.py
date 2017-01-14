adf74f13d7335552e583199611b9ba79d2aab89e
Gaurav Kshirsagar <gauravk1001@gmail.com>
2017-01-14 14:09:19 -0500

    Changed to Python3

--- get_requests.py
+++ get_requests.py
@@ -9,8 +9,8 @@ def do_something():
     # user_data = requests.get(r'http://www.reddit.com/user/' + username + '/' + u_property + '/.json')
     #
     # data = json.loads(user_data.text)
-    # print 'User:', username , '\nData:', property, '\n\n'
-    # print json.dumps(data['data']['children'][0:2], indent=4, sort_keys=True)
+    # print('User:', username , '\nData:', property, '\n\n')
+    # print(json.dumps(data['data']['children'][0:2], indent=4, sort_keys=True))
 
     # Get stuff from a sub
     subreddit = 'askreddit'
@@ -18,13 +18,13 @@ def do_something():
     sub_data = requests.get(r'http://www.reddit.com/r/' + subreddit + '/' + s_property + '/.json')
 
     data = json.loads(sub_data.text)
-    print 'Sub:', subreddit , '\nData:', s_property, '\n\n'
-    print json.dumps(data['data']['children'][0:2], indent=4, sort_keys=True)
-    print 'length of this thing:', len(data['data']['children'])
+    print('Sub:', subreddit , '\nData:', s_property, '\n\n')
+    print(json.dumps(data['data']['children'][0:2], indent=4, sort_keys=True))
+    print('length of this thing:', len(data['data']['children']))
 
     # Print something
     for post in data['data']['children'][0:2]:
-        print 'post: ', json.dumps(post, indent=4, sort_keys=True)
+        print('post: ', json.dumps(post, indent=4, sort_keys=True))
 
 if __name__ == '__main__':
     do_something()
